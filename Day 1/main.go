026504c01016b04f6bf65fef922b23bca842d802
mozxmmi <mdmozammil605@gmail.com>
2024-04-19 15:07:27 +0530

    some basic /and how to find prime num / also print patern

--- Day 1/main.go
+++ Day 1/main.go
@@ -2,7 +2,46 @@ package main
 
 import "fmt"
 
+func FindPrimeNum (num int){
+	for i:=2; i<=num; i++ {
+		if(num % i == 0){
+			fmt.Println("not a prime number")
+			break
+		}else{
+			fmt.Println("prime number")
+			break
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello World")
-	fmt.Print("Hello World")
+	//day 1 to learn about the basic of go
+	//1 to print any value you can you can use fmt.Println()
+	fmt.Println("hey i am mozammil 😁")
+	
+	// 2 data types in go first leant sting and how to declare a variable
+	// also use const
+	var name1 string = "mozammil"
+	var name2 = "mozammil"
+	name3 := "mozammil"
+	const name4 = "mozammil"
+	const name5 = "mozammil"
+	name6 := "mozammil"
+	fmt.Println(name1,name2,name3,name4,name5,name6)
+	// remember const use only for constant value 
+	
+    // make a find a prime num 
+	
+	FindPrimeNum(6)
+	// 3 start print 
+
+	var input int;
+	fmt.Scanln(&input)
+	for i:= 1; i<= input; i++{
+		for j:=1 ; j<=input ; j++{
+			fmt.Print("*")
+		}
+		fmt.Println()
+	}
+
 }
