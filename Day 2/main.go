d33f45d28d5782e8b95dfdb222f459c1c83cc9d7
mozxmmi <mdmozammil605@gmail.com>
2024-04-20 11:41:38 +0530

    complete 2 pattern

--- Day 2/main.go
+++ Day 2/main.go
@@ -1,5 +1,34 @@
 package main
 
-func main(){
-	
+import "fmt"
+
+func PrintStarONE (input int){
+	// Day 2 again practice pattern question 
+     for i:= 1; i<=input; i++{
+		for j:=1; j<=input; j++{
+			fmt.Print("*")
+		}
+		fmt.Println()
+	 } 
+}
+
+// now second printPattern 
+// 111
+// 222
+// 333
+func PrintSecondPattern (input int){
+	for i:= 1; i<=input; i++{
+		for j:=1; j<=input; j++{
+			fmt.Print(i)
+		}
+		fmt.Println()
+	}
+}
+func main() {
+	var input int
+    fmt.Println("Enter Your Num")
+	 fmt.Scanln(&input)
+
+	// PrintStarONE(input)
+	PrintSecondPattern(input)
 }
